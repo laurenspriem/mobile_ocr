4d958de068a050687cfd39a8ca0f8a609251474c
laurenspriem <git@lrns.me>
2025-10-07 12:26:13 +0530

    Fix ndk issue

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.onnx_ocr_plugin_example"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
